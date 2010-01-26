57246b935f097644f29f8d9d03be68471a095fbf
jonathandicarlo@jonathan-dicarlos-macbook-pro.local <none@none>
2010-01-26 14:40:34 -0800

    Menu item study: Created database schema for data storage.

--- testcases/menu-item-usage/menu-item-usage.js
+++ testcases/menu-item-usage/menu-item-usage.js
@@ -14,10 +14,24 @@ exports.experimentInfo = {
   versionNumber: 1
 };
 
+// How did the user activate the menu item?
+const UI_METHOD_MOUSE = 0; // clicking through menus with mouse
+const UI_METHOD_SHORTCUT = 1; // using shortcut key to activate directly
+const UI_METHOD_ALT_NAV = 2; // navigation using alt key and choosing an item
+
+const MENU_INTERACTION_ABORTED = -1;
+
 var COLUMNS = [
-  {property: "menu_id", type: TYPE_INT_32, displayName: "Menu"},
-  {property: "item_id", type: TYPE_INT_32, displayName: "Item"},
-  {property: "timestamp", type: TYPE_DOUBLE, displayName: "Timestamp"}];
+  {property: "ui_method", type: TYPE_INT_32, displayName: "UI Method",
+   displayValue: ["Mouse", "Keyboard shortcut", "Alt key navigation"]},
+  {property: "start_menu_id", type: TYPE_INT_32,
+   displayName: "Starting Menu"},
+  {property: "explore_ms", type: TYPE_INT_32, displayName: "Milliseconds to find"},
+  {property: "explore_num", type: TYPE_INT_32, displayName: "Number of menus explored"},
+  {property: "menu_id", type: TYPE_INT_32, displayName: "Menu Chosen"},
+  {property: "item_id", type: TYPE_INT_32, displayName: "Item Chosen"},
+  {property: "timestamp", type: TYPE_DOUBLE, displayName: "Time of Menu Choice",
+   displayValue: function(value) {return new Date(value).toLocaleString();}}];
 
 exports.dataStoreInfo = {
   fileName: "testpilot_menu_item_usage_results.sqlite",
@@ -82,3 +96,5 @@ exports.webContent = {
   onPageLoad: function(experiment, document, graphUtils) {
   }
 };
+
+//http://www.rgraph.net/

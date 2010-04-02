ee1af74954b2b325cbc4cc2bd6cd277064e84e47
jonathandicarlo@host-5-249.mv.mozilla.com <none@none>
2010-04-02 14:40:18 -0700

    Added a proper survey explanation to pilot background survey, broke down 'how long do you use the web' into smaller categories, and added a 'home/work/school' question.

--- testcases/pilot-background-survey.js
+++ testcases/pilot-background-survey.js
@@ -7,8 +7,13 @@ const CHECK_BOXES = 4;
 exports.surveyInfo = {
   surveyId: "basic_panel_survey",
   surveyName: "Pilot Background Survey",
-  summary: "A basic survey to help us understand who our Test Pilots are.",
-  surveyExplanation: "This survey is to understand... (edit this)",
+  summary: "A basic survey to help us understand the Test Pilot community.",
+  thumbnail: "",
+  surveyExplanation: "<p>Thank you for joining the Test Pilot team!</p> \
+<p>Please help us understand our Test Pilot community better by filling out \
+this survey. This survey doesn't contain any personally identifiable questions.\
+It will be uploaded along with any Test Pilot study data that you choose to \
+submit. You can always use the button below to review or change your answers.</p>",
   minTPVersion: "0.4",
   surveyQuestions:  [
    { question: "How long have you used Firefox?",
@@ -41,16 +46,22 @@ exports.surveyInfo = {
   { question: "How much time do you spend on the Web per day?",
     type: MULTIPLE_CHOICE,
     choices: ["Less than 1 hour",
-              "1-4 hours",
-              "4-8 hours",
-              "8-12 hours",
-              "More than 12 hours"]},
+              "1-2 hours",
+              "2-4 hours",
+              "4-6 hours",
+              "6-8 hours",
+              "8-10 hours",
+              "More than 10 hours"]},
   { question: "How would you describe your computer/web skill level?",
     type: SCALE,
     scale_minimum: 1,
     scale_maximum: 10,
     min_label: "Super low",
     max_label: "Super high"},
+  { question: "Where do you mainly use this computer?",
+    type: CHECK_BOXES_WITH_FREE_ENTRY,
+    choices: ["Home", "Work", "School"],
+    free_entry: "Other" },
    { question: "What are your main purposes for using the Web?",
      type: CHECK_BOXES_WITH_FREE_ENTRY,
      choices: ["Work: programming, coding, etc.",

3e3c456250b1e6b59e53764d0f8513a16aaba17e
jonathandicarlo@host-5-249.mv.mozilla.com <none@none>
2010-04-02 15:47:27 -0700

    Removed old 'new pilot survey' based on SurveyMonkey - not used anymore

--- testcases/new_pilot_survey.js
+++
@@ -1,6 +0,0 @@
-exports.surveyInfo = {
-  surveyId: "survey_for_new_pilots",
-  surveyName: "Survey For New Test Pilots",
-  surveyUrl: "http://www.surveymonkey.com/s.aspx?sm=bxR0HNhByEBfugh8GPASvQ_3d_3d",
-  resultsUrl: "http://www.surveymonkey.com/sr.aspx?sm=oZPWdDCVgnJqkmPERROH6AWWPcmTImSDiMyFunw16b8_3d"
-};

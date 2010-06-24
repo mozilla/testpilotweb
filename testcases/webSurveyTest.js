cabf376b4b5f89737c652d749c7d91a953fbeeda
jonathandicarlo@host-2-71.mv.mozilla.com <none@none>
2010-06-24 14:53:59 -0700

    Fixed url of test survey

--- testcases/webSurveyTest.js
+++ testcases/webSurveyTest.js
@@ -1,7 +1,7 @@
 exports.surveyInfo = {
   surveyId: "evil_brain_jono_survey",
   surveyName: "Evil Brain Jono Survey",
-  surveyUrl: "www.evilbrainjono.net",
+  surveyUrl: "http://www.evilbrainjono.net",
   summary: "A totally fake web survey for testing purposes",
   thumbnail: "https://testpilot.mozillalabs.com/testcases/pilot-survey-thumbnail.png"
 };

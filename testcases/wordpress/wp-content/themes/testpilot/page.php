33d736b04a86555a5cb87a5395aaca4d6c0e757a
Edward Lee <edilee@mozilla.com>
2010-07-22 17:33:35 -0700

    Bug 581206 - Fix testcases wordpress template [r=Mardak] Shrink comment textarea width and fix id for testcase content.

--- testcases/wordpress/wp-content/themes/testpilot/page.php
+++ testcases/wordpress/wp-content/themes/testpilot/page.php
@@ -6,7 +6,7 @@
 
 get_header(); ?>
 
-<div id="content">
+<div id="testcases-content">
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">

2892731f3e0981c11c3ca1db2bcd6ccf01eb0fcf
Jono X <jono@mozilla.com>
2010-08-24 12:57:30 -0700

    Released Add-on Marketplace Survey

---
+++ testcases/addon_marketplace_survey.js
@@ -0,0 +1,9 @@
+exports.surveyInfo = {
+  surveyId: "addon_marketplace_survey",
+  surveyName: "Addon Marketplace",
+  surveyUrl: "http://www.surveygizmo.com/s3/352177/AMO-Marketplace-For-Testpilot",
+  summary: "We're looking at how we can build a marketplace to bring Firefox Addon"
+           + " developers closer to their users, and to help them to share and to sell"
+           + " their Addons.  We would like to know how you feel about this.",
+  thumbnail: "https://testpilot.mozillalabs.com/testcases/pilot-survey-thumbnail.png"
+};

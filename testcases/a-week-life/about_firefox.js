afc50196514b68a0eeaffcff1eee4118b5d57d5a
Jono X <jono@mozilla.com>
2010-08-31 18:25:18 -0700

    About: Firefox study now collecting plugin versions, modified prefs, memory usage, and d2d settings.

--- testcases/a-week-life/about_firefox.js
+++ testcases/a-week-life/about_firefox.js
@@ -27,6 +27,73 @@ exports.dataStoreInfo = {
   ]
 };
 
+// Copied from about:support
+const PREFS_WHITELIST = [
+  "accessibility.",
+  "browser.fixup.",
+  "browser.history_expire_",
+  "browser.link.open_newwindow",
+  "browser.mousewheel.",
+  "browser.places.",
+  "browser.startup.homepage",
+  "browser.tabs.",
+  "browser.zoom.",
+  "dom.",
+  "extensions.checkCompatibility",
+  "extensions.lastAppVersion",
+  "font.",
+  "general.useragent.",
+  "gfx.color_management.mode",
+  "javascript.",
+  "keyword.",
+  "layout.css.dpi",
+  "network.",
+  "places.",
+  "print.",
+  "privacy.",
+  "security."
+];
+
+// The blacklist, unlike the whitelist, is a list of regular expressions.
+const PREFS_BLACKLIST = [
+  /^network[.]proxy[.]/,
+  /[.]print_to_filename$/
+];
+
+let gPrefService = Cc["@mozilla.org/preferences-service;1"]
+                     .getService(Ci.nsIPrefService)
+                     .QueryInterface(Ci.nsIPrefBranch2);
+
+let Application = Cc["@mozilla.org/fuel/application;1"]
+                             .getService(Ci.fuelIApplication);
+
+function getModifiedPrefs() {
+  // We use the low-level prefs API to identify prefs that have been
+  // modified, rather that Application.prefs.all since the latter is
+  // much, much slower.  Application.prefs.all also gets slower each
+  // time it's called.  See bug 517312.
+  let prefNames = getWhitelistedPrefNames();
+  let prefs = [Application.prefs.get(prefName)
+                      for each (prefName in prefNames)
+                          if (gPrefService.prefHasUserValue(prefName)
+                            && !isBlacklisted(prefName))];
+  return prefs;
+}
+
+function getWhitelistedPrefNames() {
+  let results = [];
+  PREFS_WHITELIST.forEach(function (prefStem) {
+    let prefNames = gPrefService.getChildList(prefStem);
+    results = results.concat(prefNames);
+  });
+  return results;
+}
+
+function isBlacklisted(prefName) {
+  return PREFS_BLACKLIST.some(function (re) {
+                                return re.test(prefName);});
+}
+
 function AboutFxStudyGlobalObserver() {
   // No need for a per-window constructor
   AboutFxStudyGlobalObserver.baseConstructor.call(this, null);
@@ -35,19 +102,51 @@ BaseClasses.extend(AboutFxStudyGlobalObserver,
                    BaseClasses.GenericGlobalObserver);
 AboutFxStudyGlobalObserver.prototype.onExperimentStartup = function(store) {
   AboutFxStudyGlobalObserver.superClass.onExperimentStartup.call(this, store);
+  let self = this;
 
-  // Copied from about:memory
-  let mgr = Cc["@mozilla.org/memory-reporter-manager;1"]
-        .getService(Ci.nsIMemoryReporterManager);
+  // Clear out the store each time.
+  store.wipeAllData(function() {
+    // Copied from about:memory
+    let mgr = Cc["@mozilla.org/memory-reporter-manager;1"]
+          .getService(Ci.nsIMemoryReporterManager);
 
-  let e = mgr.enumerateReporters();
-  while (e.hasMoreElements()) {
-    let mr = e.getNext().QueryInterface(Ci.nsIMemoryReporter);
-    dump("Path = " + mr.path + ", Desc = " + mr.description + ", Used = "
-         + mr.memoryUsed + "\n");
-  }
+    let e = mgr.enumerateReporters();
+    while (e.hasMoreElements()) {
+      let mr = e.getNext().QueryInterface(Ci.nsIMemoryReporter);
+      self.record({key: mr.path, value: mr.memoryUsed + " bytes"});
+    }
 
-  //this.record({event: 2, timestamp: Date.now()});
+    // Copied from about:plugins
+    let wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+                          .getService(Ci.nsIWindowMediator);
+    let frontWindow = wm.getMostRecentWindow("navigator:browser");
+    let plugins = frontWindow.navigator.plugins;
+    plugins.refresh(false);
+    for (let i = 0; i < plugins.length; i++) {
+      let plugin = plugins[i];
+      if (plugin) {
+        self.record({key: plugin.filename, value: plugin.version});
+      }
+    }
+
+    // Copied from about:support
+    let d2d;
+    try {
+      // nsIGfxInfo is currently only implemented on Windows
+      d2d = Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo).D2DEnabled;
+    } catch (e) {
+      d2d = false;
+    }
+    self.record({key: "d2d enabled", value: "" + d2d});
+
+    let prefs = getModifiedPrefs();
+    prefs.forEach(function(pref) {
+                    self.record({key: pref.name, value: "" + pref.value});
+                  });
+
+    // Could also include:  about:buildconfig if required;
+    // detailed extension data.
+  });
 };
 exports.handlers = new AboutFxStudyGlobalObserver();
 

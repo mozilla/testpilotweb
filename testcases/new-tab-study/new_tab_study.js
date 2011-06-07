31ed1d4538c139f4edea509386575fcb6f53776b
Lilian Weng <lweng@mozilla.com>
2011-06-06 17:41:30 -0700

    change the id of new-tab-study

--- testcases/new-tab-study/new_tab_study.js
+++ testcases/new-tab-study/new_tab_study.js
@@ -4,7 +4,7 @@ BaseClasses = require("study_base_classes.js");
 exports.experimentInfo = {
 
   testName: "New Tab Study",
-  testId: 1010,				//  unique
+  testId: 20110606newtabstudy		//  unique
   testInfoUrl: "https://",	// URL of page explaining your study
   summary: "Detect what the users do after open a new blank tab",
   thumbnail: "http://", // URL of image representing your study (90x90)
